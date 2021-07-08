534657ddfb7a24baf7b6dd6c3060ff328d63ecd9
Yann Pichereau <yann_pichereau@hotmail.fr>
2021-07-08 22:59:57 +0200

    add test event proposal

--- test/offering.test.js
+++ test/offering.test.js
@@ -4,7 +4,9 @@ const { expect } = require('chai');
 const Offering = artifacts.require("Offering");
 
 contract("Offering", function(accounts) {
-    const id1 = "0xf3e7C248B23020Bc1C81abac31a1Fd88A7860f73";
+    const owner = accounts[0];
+
+    const minScore = new BN(2);
     const description = "Test one two";
 
     describe("Début des tests pour Offering", function() {
@@ -12,11 +14,19 @@ contract("Offering", function(accounts) {
             this.OfferingInstance = await Offering.new();
         });
 
-        describe("Zone 1", function() {
+        describe("Proposal :", function() {
             it("Ajout d'une proposal", async function() {
-                await this.OfferingInstance.addPackage(id1, 2, description);
-                let proposals = await this.OfferingInstance.packages(0);
-                expect(proposals[2]).to.be.equal(description);
+                await this.OfferingInstance.addProposal(minScore, description);
+                let proposals = await this.OfferingInstance.proposals(0);
+                expect(proposals[0]).to.be.bignumber.equal(minScore);
+                expect(proposals[1]).to.be.equal(description);
+            });
+
+            it("Event : ProposalAdded", async function() {
+                ;
+                expectEvent(await this.OfferingInstance.addProposal(minScore, description, {from:owner}),
+                "ProposalAdded",
+                {id: new BN(0), minScoring: minScore, description: description});
             });
         });
 
